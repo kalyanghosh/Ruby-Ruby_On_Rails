497c0f5cc5d7ff9a4e4b30098ea31061c5b54506
Kalyan Ghosh <24x7kalyan@gmail.com>
2018-01-14 10:11:11 -0500

    Add files via upload

---
+++ function_chaining.rb
@@ -0,0 +1,6 @@
+name="Kalyan"
+lowercase=name.downcase
+reverse=lowercase.reverse
+upcase=reverse.upcase
+puts upcase
+puts name.downcase.reverse.upcase
---
+++ math.rb
@@ -0,0 +1,6 @@
+my_num=102
+quotient=my_num/10
+rem=my_num%10
+
+puts quotient
+puts rem
